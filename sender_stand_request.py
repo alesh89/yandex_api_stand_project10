65267291feeb6cb735df1709da0801d23a3701c0
alesh89 <37570919+alesh89@users.noreply.github.com>
2024-07-18 21:40:36 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -18,18 +18,20 @@ def post_new_user(body):
     # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
     # json=body используется для отправки данных пользователя в формате JSON
     # headers=data.headers устанавливает заголовки запроса из модуля data
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+    response_user=requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
                          json=body,
                          headers=data.headers)
+    current_token = response_user.json()['authToken']
+    auth_token= "Bearer " + current_token
+    return auth_token
 
-def post_new_client_kit(body):
+def post_new_client_kit(body,auth_token):  
+    current_header = data.headers.copy()
+    current_header["Authorization"] = auth_token
     # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
     # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
     # json=body используется для отправки данных пользователя в формате JSON
     # headers=data.headers устанавливает заголовки запроса из модуля data
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH,
                          json=body,
-                         headers=data.headers)
-
-
-
+                         headers=current_header)
